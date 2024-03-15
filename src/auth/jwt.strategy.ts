baa17194457adfd1312e354631b2d66472e1050b
raouia <raouia.daghnouj@esprit.tn>
2024-03-15 10:38:33 +0100

    Back

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from './auth.service';
-import { Users } from 'NexusBackendNestjs/dist/users/schemas/users.schema';
+
 
 
 @Injectable()
